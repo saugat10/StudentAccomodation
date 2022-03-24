38806228be24ee527e73684729b62d3f5c5d92f6
saugat10 <saugatkhatiwada10@gmail.com>
2022-03-24 11:16:13 +0100

    leasing half

--- StudentAccomodation/Services/ADOServices/ADOLeasingServices/ADOLeasing.cs
+++ StudentAccomodation/Services/ADOServices/ADOLeasingServices/ADOLeasing.cs
@@ -41,5 +41,34 @@ namespace Student_Accomodation.Services.ADOServices.ADOLeasingServices
                 return returnList;
             }
         }
+
+        public List<Leasing> GetWaitingStudent(int placeNO) {
+            List<Leasing> returnList = new List<Leasing>();
+            string query = "select top 1 Student_No  from student order by Registration_Date";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    Leasing leasing = new Leasing();
+                    leasing.Student_No = Convert.ToInt32(reader[0]);
+                    leasing.Place_No = placeNO;
+                    returnList.Add(leasing);
+                    //while (reader.Read())
+                    //{
+                    //    Leasing leasing = new Leasing();
+                    //    leasing.Leasing_No = Convert.ToInt32(reader[0]);
+                    //    leasing.Student_No = Convert.ToInt32(reader[1]);
+                    //    leasing.Place_No = Convert.ToInt32(reader[2]);
+                    //    leasing.Date_From = Convert.ToDateTime(reader[3]);
+                    //    leasing.Date_To = Convert.ToDateTime(reader[4]);
+                    //    returnList.Add(leasing);
+                    //}
+                }
+                return returnList;
+            }
+        }
     }
 }
